fe4e6512576b23ab3ce459d0cc11d106b6d4f856
Jacob Hladky <jacob.hladky@gmail.com>
2015-02-19 02:43:50 -0800

    Finished most of the README

--- release.sh
+++ release.sh
@@ -21,12 +21,8 @@ if [ ! -e "nix/ratload" ]; then
 fi
 
 mkdir $PRJ_DIR
-mkdir $PRJ_DIR/asm
 mkdir $PRJ_DIR/bin
 mkdir $PRJ_DIR/vhdl
-
-cp asm/ratload.asm $PRJ_DIR/asm/
-cp asm/serial_test.asm $PRJ_DIR/asm/
 
 cp RAT_CPU/RS232RefComp.vhd $PRJ_DIR/vhdl/
 cp RAT_CPU/ascii_to_int.vhd $PRJ_DIR/vhdl/
