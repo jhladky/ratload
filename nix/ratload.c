07e388181b06049f07bc59f195ba043703ec71c9
Jacob Hladky <jacob.hladky@gmail.com>
2015-02-20 09:24:50 -0800

    Added MIT license; Rewrote nix ratlaod program to be more similar to the windows version

--- nix/ratload.c
+++ nix/ratload.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <termios.h>
 #include <fcntl.h>
@@ -9,73 +10,114 @@
 
 #include "ratload.h"
 
-static void check_args(const int argc, const char * argv[]);
+static void parse_args(char** parsed, const int argc, char* argv[]);
+static int config_and_handshake(int* fd, const char* port);
+static int program_board(const char* fileName, const char* portName);
+static int run_serial_test(const char* portName);
 static void loop_to_array(FILE * prog_rom);
 static inline char int_to_char(const uint8_t in);
-static inline uint8_t char_to_int(const char in);
-static inline void force_quit(const char * phrase);
 
-int main(const int argc, const char * argv[]) {
-   struct termios options;
+static const char* err2Str[NUM_ERRORS + 1] = {
+   "",                                    // 0
+   "File not found.",                     // 1
+   "Invalid prog_rom file.",              // 2
+   "Bad file read.",                      // 3
+   "Bad serial device.",                  // 4
+   "Configuring serial device failed.",   // 5
+   "Handshake with Nexys board failed.",  // 6
+   "Received bad data from Nexys board.", // 7
+   "Timeout."                             // 8
+};
+
+int main(const int argc, char* argv[]) {
+   int res;
+   char* parsed[2];
+
+   parse_args(parsed, argc, argv);
+
+   if (parsed[0] == NULL) {
+      res = run_serial_test(parsed[1]);
+   } else {
+      res = program_board(parsed[0], parsed[1]);
+   }
+
+   if (res != 0) {
+      fprintf(stderr, "%s\n", err2Str[res]);
+   }
+
+   return 0;
+}
+
+static int run_serial_test(const char* portName) {
+   uint8_t confirm, i;
+   int sp, res;
+
+   if ((res = config_and_handshake(&sp, portName))) {
+      return res;
+   }
+
+   for (i = 0; i < TEST_LENGTH; i++) {
+      confirm = int_to_char(i);
+      if (write(sp, &confirm, 1) < 1) {
+         close(sp);
+         return E_BAD_DEV;
+      }
+
+      if (read(sp, &confirm, 1) < 1) {
+         close(sp);
+         return E_BAD_DEV;
+      }
+
+      if (confirm != i) {
+         return E_BAD_DATA;
+      }
+   }
+
+   return 0;
+}
+
+static int program_board(const char* fileName, const char* portName) {
    uint8_t progRomArr[PROG_ROM_LINES][PROG_ROM_SEGS], c, topC;
    char progRomProper[PROG_ROM_LINES][PROG_ROM_SEGS];
-   char confirm, start = 0X7E; //0X78 == '~'
-   FILE * prog_rom; //fix this variable name later
-   int fd, i, j, pNdx, sNdx;
+   FILE * progRom; //fix this variable name later
+   char confirm;
+   int i, j, fd, res;
 
-   check_args(argc, argv);
-
-   if (!strcmp(argv[1], "-f")) {
-      pNdx = 2;
-      sNdx = 4;
-   } else {
-      pNdx = 4;
-      sNdx = 2;
-   }
-
-   if ((prog_rom = fopen(argv[pNdx], "r")) == NULL) {
-      force_quit("Opening prog_rom failed");
-   }
-
-   if ((fd = open(argv[sNdx], O_RDWR)) == -1) {
-      force_quit("Opening serial device failed");
+   if ((progRom = fopen(fileName, "r")) == NULL) {
+      return E_NO_FILE;
    }
 
   //loop through the INIT prog_rom array
-   loop_to_array(prog_rom);
+   loop_to_array(progRom);
    for (i = 0; i < INIT_HEIGHT; i++) {
       for (j = 0; j < INIT_WIDTH; j++) {
-         c = fgetc(prog_rom);
+         c = fgetc(progRom);
          if (c >= '0' && c <= '9') {
             c -= '0';
          } else if (c >= 'A' && c <= 'F') {
             c = c - 'A' + 10;
          } else {
-            printf("Invalid prog_rom.vhd file, exiting.\n");
-            fclose(prog_rom);
-            close(fd);
-            exit(EXIT_FAILURE);
+            fclose(progRom);
+            return E_BAD_FILE;
          }
          progRomArr[(i * 16) + ((63 - j) / 4)][j % 4 + 1] = c;
       }
       //the " at the end of the string:
-      fgetc(prog_rom);
-      loop_to_array(prog_rom);
+      fgetc(progRom);
+      loop_to_array(progRom);
    }
 
    //loop through the INITP prog_rom array
    for (i = 0; i < INITP_HEIGHT; i++) {
       for (j = 0; j < INITP_WIDTH; j++) {
-         c = fgetc(prog_rom);
+         c = fgetc(progRom);
          if (c >= '0' && c <= '9') {
             c -= '0';
          } else if (c >= 'A' && c <= 'F') {
             c = c - 'A' + 10;
          } else {
-            printf("Invalid prog_rom.vhd file, exiting.\n");
-            fclose(prog_rom);
-            close(fd);
-            exit(EXIT_FAILURE);
+            fclose(progRom);
+            return E_BAD_FILE;
          }
          topC = c;
          topC &= 0x0c;
@@ -86,11 +128,11 @@ int main(const int argc, const char * argv[]) {
          progRomArr[(i * 128) + ((63 - j) * 2)][0] = c;
       }
 
-      fgetc(prog_rom);
-      loop_to_array(prog_rom);
+      fgetc(progRom);
+      loop_to_array(progRom);
    }
 
-   fclose(prog_rom);
+   fclose(progRom);
 
    //convert the instructions BACK to ASCII
    for (i = 0; i < PROG_ROM_LINES; i++) {
@@ -99,9 +141,42 @@ int main(const int argc, const char * argv[]) {
       }
    }
 
+   if ((res = config_and_handshake(&fd, portName))) {
+      return res;
+   }
+
+   fprintf(stderr, "Connection opened: sending data...");
+
+   //send the instructions to the UART
+   for (i = 0; i < 1024; i++) {
+      for (j = 4; j > -1; j--) {
+         if (write(fd, &progRomProper[i][j], 1) < 1 ||
+             read(fd, &confirm, 1) < 1) {
+            return E_BAD_DEV;
+         }
+      }
+      if (!(i % 100)) {
+         fprintf(stderr, ".");
+      }
+   }
+
+   printf("Finished!\n");
+   close(fd);
+
+   return 0;
+}
+
+static int config_and_handshake(int* sp, const char* portName) {
+   struct termios options;
+   uint8_t confirm = MAGIC_BYTE;
+
+   if ((*sp = open(portName, O_RDWR)) == -1) {
+      return E_BAD_DEV;
+   }
+
    //configuration of the serial port
-   if (tcgetattr(fd, &options) == -1) {
-      force_quit("Serial Configuration failed.");
+   if (tcgetattr(*sp, &options) == -1) {
+      return E_CONF_FAIL;
    }
 
    cfsetispeed(&options, B9600);
@@ -114,82 +189,87 @@ int main(const int argc, const char * argv[]) {
    options.c_cflag |= CS8;
    options.c_cflag &= ~CRTSCTS; //disable hardware flow control
 
-   if (tcsetattr(fd, TCSANOW, &options) == -1) {
-      force_quit("Serial Configuration failed");
+   if (tcsetattr(*sp, TCSANOW, &options) == -1) {
+      close(*sp);
+      return E_CONF_FAIL;
    }
 
-   //confirm = 0x7F;
-
-   //for debugging
-#ifdef DEBUG
-   for (i = 0; i < 32; i++) {
-      for (j = 4; j > -1; j--) {
-         printf("%c", progRomProper[i][j]);
-      }
-      printf("\n");
-   }
-#endif
-
-   //send the start byte, wait until we get it back
-   //then we can start sending the actual data
-   if (write(fd, &start, 1) < 1 ||
-       read(fd, &confirm, 1) < 1) {
-      force_quit("Error communicating with Nexys2 board.\n");
+   if (write(*sp, &confirm, 1) < 1) {
+      close(*sp);
+      return E_BAD_DEV;
    }
 
-   fprintf(stderr, "Connection opened: sending data...");
+   confirm = 0;
 
-   //send the instructions to the UART
-   for (i = 0; i < 1024; i++) {
-      for (j = 4; j > -1; j--) {
-         if (write(fd, &progRomProper[i][j], 1) < 1 ||
-             read(fd, &confirm, 1) < 1) {
-            force_quit("Error communicating with Nexys2 board.\n");
-         }
-      }
-      if (!(i % 100)) {
-         fprintf(stderr, ".");
-      }
+   if (read(*sp, &confirm, 1) < 1) {
+      close(*sp);
+      return E_BAD_DEV;
    }
 
-   printf("Finished!\n");
-   close(fd);
+   if (confirm != MAGIC_BYTE) {
+      close(*sp);
+      return E_HANDSHAKE;
+   }
 
-   return EXIT_SUCCESS;
+   return 0;
 }
 
-//make sure we are specifying -f \path\to\file
-static void check_args(const int argc, const char * argv[]) {
-   if (argc != NUM_ARGS) {
-      printf("Too %s arguments.\n", argc > NUM_ARGS ? "many" : "few");
-      printf("Please specify a file (-f) and serial TTY (-d)\n");
+static void parse_args(char** parsed, const int argc, char* argv[]) {
+   bool fFound = false, dFound = false, lFound = false,
+      hFound = false, tFound = false;
+   int i;
+
+   for (i = 0; i < argc; i++) {
+      if (i < argc - 1 &&
+          (!strcmp(argv[i], "-f") || !strcmp(argv[i], "--file"))) {
+         fFound = true;
+         parsed[0] = argv[i + 1];
+         i++;
+      } else if (i < argc - 1 &&
+                 (!strcmp(argv[i], "-d") || !strcmp(argv[i], "--device"))) {
+         dFound = true;
+         parsed[1] = argv[i + 1];
+         i++;
+      } else if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+         hFound = true;
+      } else if (!strcmp(argv[i], "-l") || !strcmp(argv[i], "--list")) {
+         lFound = true;
+      } else if (!strcmp(argv[i], "-t") || !strcmp(argv[i], "--test")) {
+         tFound = true;
+      }
+   }
+
+   if (!hFound && lFound) {
+      printf("Serial port listing not supported on this platform.\n");
+      exit(EXIT_SUCCESS);
+   } else if (hFound || (!tFound && (!fFound || !dFound)) ||
+              (tFound && !dFound) || (tFound && fFound)) {
+      printf("Usage: ratload -d|--device <serial device> "
+             "-f|--file <prog_rom file>\n"
+             "       (program Nexys2 board)\n"
+             "   or  ratload -d|--device <serial device> -t|--test\n"
+             "       (run serial connection test)\n"
+             "   or  ratload -l|--list\n"
+             "       (list available serial devices)\n"
+             "   or  ratload -h|--help\n"
+             "       (print this message)\n");
       exit(EXIT_FAILURE);
-   } else if ((strcmp(argv[1], "-f") && strcmp(argv[3], "-f")) ||
-              (strcmp(argv[1], "-d") && strcmp(argv[3], "-d"))) {
-      printf("Option not supported. Only -f and -d supported\n");
-      exit(EXIT_FAILURE);
+   }
+
+   if (tFound) {
+      parsed[0] = NULL;
    }
 }
 
 //loop to get to the beginning of the data
-static void loop_to_array(FILE * prog_rom) {
+static void loop_to_array(FILE * progRom) {
    char a = ' ';
 
    while (a != '"') {
-      a = fgetc(prog_rom);
+      a = fgetc(progRom);
    }
 }
 
 static inline char int_to_char(const uint8_t in) {
    return in + (in <= 9 ? '0' : ('A' - 10));
-}
-
-static inline uint8_t char_to_int(const char in) {
-   //fill me out
-   return in;
-}
-
-static inline void force_quit(const char* phrase) {
-   perror(phrase);
-   exit(EXIT_FAILURE);
 }
--- nix/ratload.h
+++ nix/ratload.h
@@ -9,5 +9,19 @@
 #define INIT_WIDTH 64
 #define INITP_HEIGHT 8
 #define INITP_WIDTH 64
+#define MAGIC_BYTE 0X7E
+#define TEST_LENGTH 16
+
+///errors///
+#define E_NO_FILE      1
+#define E_BAD_FILE     2
+#define E_BAD_READ     3
+#define E_BAD_DEV      4
+#define E_CONF_FAIL    5
+#define E_HANDSHAKE    6
+#define E_BAD_DATA     7
+#define E_TIMEOUT      8
+
+#define NUM_ERRORS     8
 
 #endif
